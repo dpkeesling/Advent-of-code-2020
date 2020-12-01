86e0521b9797568608c15ed9827a00707428143f
Dan Keesling <dpkeesling@gmail.com>
2020-12-01 10:26:40 -0500

    Fixed declarations of loop counters

--- day1/main_part_two.c
+++ day1/main_part_two.c
@@ -5,16 +5,13 @@ int main(){
     FILE *fptr;
     fptr = fopen("./input.txt","r");
     int numberArray[200];
-    int i;
-    for (i = 0; i < 200; i++){
+    for (int i = 0; i < 200; i++){
         fscanf(fptr, "%d", &numberArray[i]);
     }
     bool valueFound = false;
-    for(i = 0; i < 200 && valueFound == false; i++){
-        int j;
-        for(j = 0; j < 200 && valueFound == false; j++){
-            int k;
-            for(k = 0; k < 200 && valueFound == false; k++) {
+    for(int i = 0; i < 200 && valueFound == false; i++){
+        for(int j = 0; j < 200 && valueFound == false; j++){
+            for(int k = 0; k < 200 && valueFound == false; k++) {
                 if(numberArray[j] != numberArray[i] && numberArray[k] != numberArray[i] && numberArray[k] != numberArray[j]) {
                     if(numberArray[j] + numberArray[i] + numberArray[k] == 2020){
                         printf("The sum that is 2020's product is %d\n", numberArray[j] * numberArray[i] * numberArray[k]);
